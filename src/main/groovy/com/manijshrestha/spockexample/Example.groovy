d247908489d0d648bfa0f3cb973f5d8f3575f21e
Manij Shrestha <manijshrestha@ubuntu.ubuntu-domain>
2011-10-12 00:19:39 -0500

    initial commit

---
+++ src/main/groovy/com/manijshrestha/spockexample/Example.groovy
@@ -0,0 +1,15 @@
+//
+// Generated from archetype; please customize.
+//
+
+package com.manijshrestha.spockexample
+
+/**
+ * Example Groovy class.
+ */
+class Example
+{
+    def show() {
+        println 'Hello World'
+    }
+}
---
+++ src/test/groovy/com/manijshrestha/spockexample/ExampleTest.groovy
@@ -0,0 +1,21 @@
+package com.manijshrestha.spockexample
+
+import com.manijshrestha.spockexample.Example
+import org.junit.Test
+
+//
+// Generated from archetype; please customize.
+//
+
+
+/**
+ * Tests for the {@link Example} class.
+ */
+class ExampleTest
+    extends GroovyTestCase
+{
+   @Test 
+   void testShow() {
+        new Example().show()
+    }
+}
